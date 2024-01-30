b72bcd449eb75b637a350e3efefe80d0308b9787
keyurparalkar <keur.plkar@gmail.com>
2024-01-30 13:28:07 +0530

    feat: enable recursion

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,32 +1,66 @@
+const timelineData: PointProps[] = [
+  {
+    year: 1820,
+    text: "Test 1",
+  },
+  {
+    year: 1930,
+    text: "Test 2",
+  },
+  {
+    year: 1945,
+    text: "Test 3",
+  },
+  {
+    year: 2000,
+    text: "Test 4",
+  },
+  {
+    year: 2002,
+    text: "Test 5",
+  },
+];
+
 type PointProps = {
-  pointText?: string;
-  endText?: string;
+  year?: number;
+  text?: string;
+};
+
+type DrawPointsProp = {
+  pointers: PointProps[];
 };
 
 const Point = (props: PointProps) => {
-  const { pointText = "", endText = "" } = props;
+  const { year = 0, text = "" } = props;
   return (
-    <div
-      className={`container-${pointText}`}
-      style={{ display: "inline-block" }}
-    >
-      <span>{endText}</span>
+    <div className={`container-${year}`} style={{ display: "inline-block" }}>
+      <span>{text}</span>
       <svg viewBox="0 0 50 50">
         <line x1={25} y1={0} x2={25} y2={39} stroke="white" />
         <circle cx={25} cy={44} r={5} stroke="white" fill="#242424" />
       </svg>
-      <span>{pointText}</span>
+      <span>{year}</span>
     </div>
+  );
+};
+
+const DrawPoints = (props: DrawPointsProp) => {
+  const { pointers } = props;
+  if (pointers.length === 0) {
+    return <></>;
+  }
+  return (
+    <>
+      <Point {...pointers[0]} />
+      {<DrawPoints pointers={pointers.slice(1)} />}
+    </>
   );
 };
 
 const Timeline = () => {
   return (
     <div>
-      <Point endText="Hola" pointText="1924" />
-      <Point endText="Hola" pointText="1924" />
-      <Point endText="Hola" pointText="1924" />
-      <Point endText="Hola" pointText="1924" />
+      <DrawPoints pointers={timelineData} />
     </div>
   );
 };
